e82346758a696658111a17f6c206ff4f7f385b5c
samuelcney <samuelcney2@gmail.com>
2025-03-05 13:43:35 -0300

    configuring CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ configDotenv();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 5000);
 }
 
