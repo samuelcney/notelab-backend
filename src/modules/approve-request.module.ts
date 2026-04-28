4d7ace10af05f7017dc39c95d1ccf7d7d5f70e78
samuelcney <samuelcney2@gmail.com>
2026-04-28 20:39:50 -0300

    chore: update dependencies and devDependencies in package.json

--- src/modules/approve-request.module.ts
+++ src/modules/approve-request.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersModule } from 'src/modules/users.module';
-import { SupabaseStorageService } from 'src/services/supabase-s3.service';
+
+import { SupabaseStorageService } from '../services/supabase-s3.service';
 import { ApproveRequestController } from '../controllers/approve-request.controller';
 import { ApproveRequestRepository } from '../repositories/approve-request.repo';
 import { ApproveRequestService } from '../services/approve-request.service';
 import { PrismaModule } from './prisma.module';
+import { UsersModule } from './users.module';
 
 @Module({
   imports: [PrismaModule, UsersModule],
